db7a6968804f205c7223b8af8d3ee10820a1ca33
francids <francisco.mc.ids@gmail.com>
2024-09-24 15:19:49 -0400

    ui: update main scaffold layout refactor overlay style, change ftheme to zinc, update paddings

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,14 @@ import 'package:anaquel/screens/profile_screen.dart';
 import 'package:anaquel/screens/schedules_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   runApp(const Application());
 }
 
@@ -40,101 +48,110 @@ class _ApplicationState extends State<Application> {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: Brightness.dark,
-        systemNavigationBarColor: Colors.transparent,
-      ),
-    );
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     return MaterialApp(
       title: "Anaquel",
       locale: const Locale('es'),
       debugShowCheckedModeBanner: false,
       builder: (context, child) {
         return FTheme(
-          data: FThemes.red.light,
+          data: FThemes.zinc.light,
           child: child!,
         );
       },
       home: FScaffold(
-        header: FHeader(
-          title: Text(_titles[index]),
-          style: FRootHeaderStyle(
-            titleTextStyle: TextStyle(
-              fontSize: 24,
-              color: index == 0
-                  ? const Color(0xFF941932)
-                  : const Color(0xFF050505),
-              fontWeight: index == 0 ? FontWeight.w800 : FontWeight.w600,
-            ),
-            actionSpacing: 16,
-            actionStyle: FHeaderActionStyle(
-              disabledColor: const Color(0xFF050505),
-              enabledColor: const Color(0xFF050505),
-              size: 22,
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-          ),
-        ),
-        contentPad: false,
-        content: _screens[index],
-        footer: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: FBottomNavigationBar(
-            style: FBottomNavigationBarStyle(
-              item: FBottomNavigationBarItemStyle(
-                activeIconColor: const Color(0xFF050505),
-                activeTextStyle: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 14,
-                  color: Color(0xFF050505),
-                ),
-                inactiveIconColor: const Color(0xFF232323),
-                inactiveTextStyle: const TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14,
-                  color: Color(0xFF232323),
-                ),
-                iconSize: 22,
-                padding: const EdgeInsets.only(
-                  top: 8,
-                  bottom: 16,
-                ),
-              ),
-              decoration: BoxDecoration(
-                backgroundBlendMode: BlendMode.overlay,
-                color: const Color(0xFFFBFBFB),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    offset: const Offset(0, -1),
-                  ),
-                ],
-              ),
-            ),
-            index: index,
-            onChange: (index) => setState(() => this.index = index),
-            items: [
-              FBottomNavigationBarItem(
-                icon: FAssets.icons.home,
-                label: 'Principal',
-              ),
-              FBottomNavigationBarItem(
-                icon: FAssets.icons.book,
-                label: "Libros",
-              ),
-              FBottomNavigationBarItem(
-                icon: FAssets.icons.alarmClock,
-                label: "Horarios",
-              ),
-              FBottomNavigationBarItem(
-                icon: FAssets.icons.user,
-                label: 'Perfil',
+        header: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                offset: const Offset(0, 1),
               ),
             ],
           ),
+          child: FHeader(
+            title: Text(_titles[index]),
+            key: ValueKey(index),
+            style: FRootHeaderStyle(
+              titleTextStyle: TextStyle(
+                fontSize: 24,
+                color: index == 0
+                    ? const Color(0xFF941932)
+                    : const Color(0xFF050505),
+                fontWeight: index == 0 ? FontWeight.w800 : FontWeight.w600,
+              ),
+              actionSpacing: 16,
+              actionStyle: FHeaderActionStyle(
+                disabledColor: const Color(0xFF050505),
+                enabledColor: const Color(0xFF050505),
+                size: 22,
+              ),
+              padding: const EdgeInsets.only(
+                left: 16,
+                right: 16,
+                top: 8,
+                bottom: 16,
+              ),
+            ),
+          ),
+        ),
+        contentPad: true,
+        content: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: _screens[index],
+        ),
+        footer: FBottomNavigationBar(
+          style: FBottomNavigationBarStyle(
+            item: FBottomNavigationBarItemStyle(
+              activeIconColor: const Color(0xFF050505),
+              activeTextStyle: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 14,
+                color: Color(0xFF050505),
+              ),
+              inactiveIconColor: const Color(0xFF232323),
+              inactiveTextStyle: const TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                color: Color(0xFF232323),
+              ),
+              iconSize: 22,
+              padding: const EdgeInsets.only(
+                top: 8,
+                bottom: 8,
+              ),
+            ),
+            decoration: BoxDecoration(
+              backgroundBlendMode: BlendMode.overlay,
+              color: const Color(0xFFFBFBFB),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  offset: const Offset(0, -1),
+                ),
+              ],
+            ),
+          ),
+          index: index,
+          onChange: (index) => setState(() => this.index = index),
+          items: [
+            FBottomNavigationBarItem(
+              icon: FAssets.icons.home,
+              label: 'Principal',
+            ),
+            FBottomNavigationBarItem(
+              icon: FAssets.icons.book,
+              label: "Libros",
+            ),
+            FBottomNavigationBarItem(
+              icon: FAssets.icons.alarmClock,
+              label: "Horarios",
+            ),
+            FBottomNavigationBarItem(
+              icon: FAssets.icons.user,
+              label: 'Perfil',
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/schedules_screen.dart
+++ lib/screens/schedules_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
 
 class SchedulesScreen extends StatelessWidget {
   const SchedulesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        FButton(
+          onPress: () {},
+          style: FButtonStyle.primary,
+          label: const Text("Crear hora de lectura"),
+        ),
+        const FDivider(),
+      ],
+    );
   }
 }
