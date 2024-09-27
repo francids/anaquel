d2018e40b7fd05a9335d14281c546cb17177f99e
francids <francisco.mc.ids@gmail.com>
2024-09-27 12:36:33 -0400

    ui: create mini tab widget

--- lib/screens/principal_screen.dart
+++ lib/screens/principal_screen.dart
@@ -1,4 +1,6 @@
 import 'package:anaquel/widgets/books/medium_book_card.dart';
+import 'package:anaquel/widgets/books/small_book_card.dart';
+import 'package:anaquel/widgets/mini_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
 
@@ -7,6 +9,25 @@ List<String> _bookCovers = [
   "https://images.squarespace-cdn.com/content/v1/624da83e75ca872f189ffa42/aa45e942-f55d-432d-8217-17c7d98105ce/image001.jpg",
   "https://images.squarespace-cdn.com/content/v1/5fc7868e04dc9f2855c99940/32f738d4-e4b9-4c61-bfc0-e813699cdd3c/laura-barrett-illustrator-beloved-girls-book-cover.jpg",
   "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/yellow-the-mind-of-a-leader-book-cover-design-template-f3cc2996f5f381d3d2d092e2d67337a4_screen.jpg"
+];
+
+List<String> _bookTitles = [
+  "SOUL",
+  "really good, actually",
+  "The Beloved Girls",
+  "The Mind of a Leader",
+];
+
+List<String> _bookAuthors = [
+  "Olivia Wilson",
+  "Monica Heisey",
+  "Harriet Evans",
+  "Nouah Schumacher",
+];
+
+List<String> _bookStatus = [
+  "Leyendo",
+  "Por leer",
 ];
 
 class PrincipalScreen extends StatelessWidget {
@@ -18,11 +39,11 @@ class PrincipalScreen extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 16),
       child: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
-                SizedBox(
+                const SizedBox(
                   width: double.infinity,
                   child: Text(
                     "Tu progreso",
@@ -32,8 +53,8 @@ class PrincipalScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                FDivider(),
-                SizedBox(
+                const FDivider(),
+                const SizedBox(
                   width: double.infinity,
                   child: Text(
                     "Tus libros",
@@ -43,6 +64,23 @@ class PrincipalScreen extends StatelessWidget {
                     ),
                   ),
                 ),
+                const SizedBox(height: 12),
+                SizedBox(
+                  width: double.infinity,
+                  child: Wrap(
+                    alignment: WrapAlignment.start,
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: [
+                      for (final status in _bookStatus)
+                        MiniTab(
+                          label: status,
+                          isSelected: status == "Leyendo",
+                        ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 8),
               ],
             ),
           ),
@@ -63,12 +101,12 @@ class PrincipalScreen extends StatelessWidget {
               },
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
-                FDivider(),
-                SizedBox(
+                const FDivider(),
+                const SizedBox(
                   width: double.infinity,
                   child: Text(
                     "Recomendaciones",
@@ -76,6 +114,25 @@ class PrincipalScreen extends StatelessWidget {
                       fontSize: 22,
                       fontWeight: FontWeight.w600,
                     ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    padding: const EdgeInsets.all(0),
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(height: 8),
+                    itemCount: _bookTitles.length,
+                    itemBuilder: (context, index) {
+                      return SmallBookCard(
+                        image: _bookCovers[index],
+                        title: _bookTitles[index],
+                        author: _bookAuthors[index],
+                      );
+                    },
                   ),
                 ),
               ],
---
+++ lib/widgets/mini_tab.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MiniTab extends StatelessWidget {
+  const MiniTab({super.key, required this.label, this.isSelected = false});
+
+  final String label;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 12,
+        vertical: 8,
+      ),
+      decoration: BoxDecoration(
+        color: isSelected ? const Color(0xFFEEEEEE) : Colors.transparent,
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Text(
+        label,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+}
