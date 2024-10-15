e58d11385642936f1aede33fc1d58a6f8c383983
francids <francisco.mc.ids@gmail.com>
2024-10-15 14:12:35 -0400

    chore: set the scale factor in the scaled widgets binding

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:anaquel/app/app.dart';
 import 'package:anaquel/constants/system_ui_config.dart';
 
 void main() {
-  ScaledWidgetsFlutterBinding.ensureInitialized();
+  ScaledWidgetsFlutterBinding.ensureInitialized(scaleFactor: (_) => 1.0);
   configureSystemUI();
   runApp(const AnaquelApp());
 }
