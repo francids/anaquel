e3c7e861b1722aec17312c63abe384fb498a4002
francids <francisco.mc.ids@gmail.com>
2024-11-22 10:19:13 -0400

    ui: show collection name and color in collection screen

--- lib/screens/collection_screen.dart
+++ lib/screens/collection_screen.dart
@@ -1,7 +1,9 @@
+import 'package:anaquel/data/models/collection.dart';
 import 'package:anaquel/logic/collections_bloc.dart';
 import 'package:anaquel/logic/user_books_bloc.dart';
 import 'package:anaquel/widgets/books/large_book_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:forui/forui.dart';
 import 'package:go_router/go_router.dart';
@@ -18,6 +20,15 @@ class CollectionScreen extends StatefulWidget {
 class _CollectionScreenState extends State<CollectionScreen>
     with SingleTickerProviderStateMixin {
   late FPopoverController popoverController;
+  String collectionName = "Colección";
+  String collectionColor = "#000000";
+
+  updateScreenHeader(String title, String color) {
+    setState(() {
+      collectionName = title;
+      collectionColor = color;
+    });
+  }
 
   @override
   initState() {
@@ -35,7 +46,32 @@ class _CollectionScreenState extends State<CollectionScreen>
   Widget build(BuildContext context) {
     return FScaffold(
       header: FHeader.nested(
-        title: const Text("Colección"),
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Stack(
+            children: [
+              Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Color(
+                    int.parse(
+                      "0xFF${collectionColor.replaceAll("#", "")}",
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Text(
+                  collectionName,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ),
         prefixActions: [
           FHeaderAction.back(
             onPress: () => context.pop(),
@@ -120,7 +156,19 @@ class _CollectionScreenState extends State<CollectionScreen>
             if (state is CollectionsLoaded) {
               var collection = state.collections.firstWhere(
                 (collection) => collection.id == widget.collectionId,
+                orElse: () => Collection(
+                  id: 0,
+                  name: "Colección",
+                  color: "#000000",
+                  books: [],
+                ),
               );
+              SchedulerBinding.instance.addPostFrameCallback((_) {
+                updateScreenHeader(
+                  collection.name,
+                  collection.color,
+                );
+              });
               if (collection.books.isEmpty) {
                 return const Center(
                   child: Text("No hay libros en esta colección"),
