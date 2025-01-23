275780b97f442c777e2b187ee1b956403b824a98
francids <francisco.mc.ids@gmail.com>
2025-01-23 12:36:43 -0400

    ui: implement intl in genres

--- lib/widgets/chip.dart
+++ lib/widgets/chip.dart
@@ -1,5 +1,58 @@
 import 'package:anaquel/constants/colors.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+
+List<String> _anaquelGenres = [
+  "Self Help",
+  "Action",
+  "Adventure",
+  "Biography",
+  "Classics",
+  "Science Fiction",
+  "True Crime",
+  "Fantasy",
+  "Horror",
+  "Mystery",
+  "Romance",
+  "Theory",
+  "Informational",
+  "Spiritual",
+  "Educational",
+  "Gambling",
+  "Dystopian",
+  "Epic",
+  "Historical Fiction",
+  "Crime Fiction",
+  "Speculative Fiction",
+  "Humor",
+  "Young Adult",
+  "Medical/Forensic",
+  "Military",
+  "Nonfiction Narrative",
+  "New Adult",
+  "Police Procedural",
+  "Post-Apocalyptic",
+  "Psychological",
+  "Magical Realism",
+  "Satire",
+  "Supernatural",
+  "Suspense",
+  "Thriller",
+  "Urban",
+  "Western",
+  "Novel",
+];
+
+String convertGenreAnaquel(String original) {
+  if (_anaquelGenres.contains(original)) {
+    return "genres.$original"
+        .toLowerCase()
+        .replaceAll(" ", "_")
+        .replaceAll("/", "_")
+        .tr();
+  }
+  return original;
+}
 
 class AChip extends StatelessWidget {
   const AChip({super.key, required this.label});
@@ -17,7 +70,9 @@ class AChip extends StatelessWidget {
         color: AppColors.antiFlashWhite,
         borderRadius: BorderRadius.circular(6),
       ),
-      child: Text(label),
+      child: Text(
+        convertGenreAnaquel(label),
+      ),
     );
   }
 }
